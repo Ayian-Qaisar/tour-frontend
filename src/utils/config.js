fad4e75c223c0e5ff328e6d68726f14051cee898
Ayian Qaisar <ayianqaisar20@gmail.com>
2024-07-05 22:41:27 +0500

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = "https://shy-pear-lemur-veil.cyclic.app/api/v1";
-export const BASE_URL = "https://tour-backend-production-88af.up.railway.app";
+export const BASE_URL = "https://tour-backend-production-88af.up.railway.app/api/v1";
